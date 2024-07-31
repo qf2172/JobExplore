758543a6f0495931cb2cc103cbf9052e267587e9
qf2172 <qf2172@columbia.edu>
2024-07-31 13:35:10 -0400

    user profile fix

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ import { StatusCodes } from "http-status-codes"
 import UserModel from "../models/UserModel.js"
 import JobModel from "../models/JobModel.js";
 import cloudinary from 'cloudinary'
-import { promises as fs } from 'fs'
+import { formatImage } from "../middleware/multerMiddleware.js";
 
 export const getCurrentUser = async (req, res) => {
     const user = await UserModel.findOne({_id: req.user.userId})
@@ -18,14 +18,14 @@ export const updateUser = async (req, res) => {
     const newUser = {...req.body}
     delete newUser.password
     if(req.file) {
-        const response = await cloudinary.v2.uploader.upload(req.file.path)
-        await fs.unlink(req.file.path)
+        const file = formatImage(req.file)
+        const response = await cloudinary.v2.uploader.upload(file)
         newUser.avatar = response.secure_url
         newUser.avatarPublicId = response.public_id
     }
-    if(req.file && updateUser.avatarPublicId) {
+    const updatedUser = await UserModel.findByIdAndUpdate(req.user.userId, newUser)
+    if(req.file && updatedUser.avatarPublicId) {
         await cloudinary.v2.uploader.destroy(updateUser.avatarPublicId)
     }
-    const updatedUser = await UserModel.findByIdAndUpdate(req.user.userId, newUser)
     res.status(StatusCodes.OK).json({user: updatedUser})
 }
--- middleware/multerMiddleware.js
+++ middleware/multerMiddleware.js
@@ -1,14 +1,28 @@
 import multer from "multer"
-
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'public/uploads')
-    },
-    filename: (req, file, cb) => {
-        const fileName = file.originalname
-        cb(null, fileName)
-    }
-})
+import DataParser from 'datauri/parser.js'
+import path from 'path'
+// disk storage
+//const storage = multer.diskStorage({
+//    destination: (req, file, cb) => {
+//        cb(null, 'public/uploads')
+//    },
+//    filename: (req, file, cb) => {
+//        const fileName = file.originalname
+//        cb(null, fileName)
+//    }
+//})
+// store image on Cloudinary
+// use buffer
+const storage = multer.memoryStorage()
 
 const upload = multer({ storage })
+const parser = new DataParser()
+export const formatImage = (file) => {
+    console.log("file:",file)
+    const fileExtension = path.extname(file.originalname).toString()
+    console.log(fileExtension)
+    const content1 = parser.format(fileExtension, file.buffer).content
+    console.log(content1)
+    return content1
+}
 export default upload
